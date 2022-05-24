7b12e856ebf6f912d373e2ab2a8dea6a043a4104
darap1 <darap@mail.ru>
2022-05-24 16:10:16 +0200

    fix : fix files and directory paths

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname,'secret-folder\\');
+const filePath = path.join(__dirname,'secret-folder');
 
 
 fs.readdir(filePath, { withFileTypes: true },function (err, files) {
         files.forEach(file => {
             if (!file.isDirectory()) {
-                fs.stat(filePath + file.name, (err, stats) => {
+                fs.stat(path.join(filePath,file.name), (err, stats) => {
                     console.log(file.name.split('.')[0], path.extname(file.name).slice(1),stats.size);//callback for stats
                 });
             }
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname,'styles\\');
-const fileDist = path.join(__dirname,'project-dist\\bundle.css');
+const filePath = path.join(__dirname,'styles');
+const fileDist = path.join(__dirname,'project-dist','bundle.css');
 
 const writeToFile = fs.createWriteStream(fileDist); //tast 2
 
@@ -10,7 +10,7 @@ fs.readdir(filePath, { withFileTypes: true },function (err, files) {    //task 3
     files.forEach(file => {
         if (!file.isDirectory()) {
                 if(path.extname(file.name).slice(1) === 'css') {
-                         fs.ReadStream(filePath + file.name).on('data', async (chunk) => {   //2
+                         fs.ReadStream(path.join(filePath,file.name)).on('data', async (chunk) => {   //2
                                 writeToFile.write(chunk);
                          });
                  }
