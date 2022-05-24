bc19c042febedcc1c2e90cbaa6744badf266418b
darap1 <darap@mail.ru>
2022-05-24 23:53:24 +0200

    feat : fix sync files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
 const path = require('path');
-const src = path.join(__dirname,'\\files\\');
-const dist = path.join(__dirname,'\\filesCopy\\');
+const src = path.join(__dirname,'files');
+const dist = path.join(__dirname,'filesCopy');
 
 
 
 async function copyDir(src, dist) {
-  try {
-    await fs.promises.mkdir(dist, { recursive: true }).catch(console.error);
+  await fs.promises.mkdir(dist, { recursive: true }).catch(console.error);
+   
         for(let file of await fs.promises.readdir(src, {withFileTypes: true})){
-         let srcFile = src + file.name;
-            let distFile = dist + file.name;
+         let srcFile =  path.join(src,file.name);
+            let distFile = path.join(dist, file.name);
                  if (file.isDirectory()) {
                      copyDir(srcFile + '/', distFile + '/');
                       }
                 else {await fs.promises.copyFile(srcFile, distFile, fs.constants.COPYFILE_EXCL);}
          }
-    }
-    catch{(console.log('directory already exists'))}
+   
 }
 
- copyDir(src, dist);
+ (async function() { 
+    await fs.promises.rm(dist, {recursive: true, force: true}).catch(console.error);
+    await copyDir(src, dist);}
+  )();
     
     
   
